f77d105fb55fc54d6de03d05e5b7288edd481297
Rick South <rick@natamus.com>
2023-12-12 10:38:38 +0100

    Creative Block Replacer source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/creativeblockreplacer/neoforge/events/NeoForgeReplaceEvent.java
@@ -0,0 +1,33 @@
+package com.natamus.creativeblockreplacer.neoforge.events;
+
+import com.natamus.creativeblockreplacer.events.ReplaceEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeReplaceEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(Phase.START)) {
+			return;
+		}
+
+		ReplaceEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onBlockClick(PlayerInteractEvent.RightClickBlock e) {
+		if (!ReplaceEvent.onBlockClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+}
