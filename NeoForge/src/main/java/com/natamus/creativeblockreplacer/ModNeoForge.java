ded17cc9dafe75d585a0d8a00f2b4cacbc7af987
Rick South <rick@natamus.com>
2024-06-14 05:37:57 +0200

    Creative Block Replacer source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/creativeblockreplacer/ModNeoForge.java
@@ -0,0 +1,30 @@
+package com.natamus.creativeblockreplacer;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.creativeblockreplacer.neoforge.events.NeoForgeReplaceEvent;
+import com.natamus.creativeblockreplacer.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeReplaceEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
