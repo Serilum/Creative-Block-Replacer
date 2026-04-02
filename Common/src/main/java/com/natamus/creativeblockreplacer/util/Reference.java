2bda1f0dde48186b70ab8348786efe3255b19600
Rick South <rick@natamus.com>
2026-04-02 16:17:00 +0200

    Creative Block Replacer source code for 26.1.1.

---
+++ Common/src/main/java/com/natamus/creativeblockreplacer/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.creativeblockreplacer.util;
+
+public class Reference {
+	public static final String MOD_ID = "creativeblockreplacer";
+	public static final String NAME = "Creative Block Replacer";
+	public static final String VERSION = "2.6";
+	public static final String ACCEPTED_VERSIONS = "[26.1.1]";
+}
