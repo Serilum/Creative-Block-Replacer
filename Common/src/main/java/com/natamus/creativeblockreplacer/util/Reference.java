f0f1bbdcb3f7f13fbee431ceecc7210f0b04b887
Rick South <rick@natamus.com>
2026-06-19 00:04:24 +0200

    Updated Creative Block Replacer to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/creativeblockreplacer/util/Reference.java
+++ Common/src/main/java/com/natamus/creativeblockreplacer/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "creativeblockreplacer";
 	public static final String NAME = "Creative Block Replacer";
 	public static final String VERSION = "2.7";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
